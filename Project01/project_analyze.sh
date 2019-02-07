d9655f947dbfff759dbba0144c9702c5488f6b38
Afzal Chishti <chishtia@mcmaster.ca>
2019-02-07 17:36:42 +0000

    decided and started creating features

---
+++ Project01/project_analyze.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+bash=0
+hask=0
+python=0
+css=0
+javascript=0
+html=0
+#add all features of the script
+echo "The list of features of this script is ..."
+echo "Type which script to execute:"
+read command
+echo "I'm executing" $command
+#execute said command
+
+#file type count
+#go through all the files in repo and count how many they are
+
+#add commands/lines before this to find all the python and haskell files and run them
+#  2> compile_fail.log
+#
+
