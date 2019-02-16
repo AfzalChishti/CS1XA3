934afd224b9cc3b510174bd992d11534c4068f41
Afzal Chishti <chishtia@mcmaster.ca>
2019-02-16 00:13:41 +0000

    Project1 Part 1

---
+++ Project01/README.md
@@ -0,0 +1,10 @@
+
+#My Functions:
+#(1) check_TODO
+
+#Takes files containing #TODO and adds them to .log
+
+#(2) check_errors
+
+#Finds all haskell files and executes the command given to check for syntax errors on them. If errors found, adds them to error.log
+
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,17 +1,58 @@
 #!/bin/bash
 
+cd ..
+#change it a bit/understand it
+function check_todo() { 
+grep -Hr "#TODO" --exclude="todo.log" "./Project01" > "./Project01/logs/todo.log"
+}
+
+function check_errors () {
+find -name "*.hs" | xargs -I {} ghc -fno-code {} &> /home/chishtia/CS1XA3/Project01/logs/compile_fail.log
+find -name "*.py" | xargs -I {} ghc -fno-code {} &> /home/chishtia/CS1XA3/Project01/logs/compile_fail.log
+}
+
+
+#change it up, so to ensure that log files are created new everytime 
+#program is run
+file1="./Project01/logs/todo.log"
+file2="./Project01/logs/compile_fail.log"
+#file3="/home/chishtia/new_csv/csv"
+if [ -e "$file1" ] || [ -e "$file2" ]; then
+        rm -r "$file1"
+        rm -r "$file2"
+fi
+
+touch ./Project01/logs/todo.log
+touch ./Project01/logs/compile_fail.log
+
+#variables to store number of specific type of files
 bash=0
 hask=0
 python=0
 css=0
 javascript=0
 html=0
-#add all features of the script
+
+#add the haskell and python error command after testing
+declare -a features=('todo' 'check_errors');
+
+#add all features of the script by printing out values of the array of 
+#commands
 echo "The list of features of this script is ..."
-echo "Type which script to execute:"
+echo ${features[@]}
+echo "Type which function to execute:"
 read command
-echo "I'm executing" $command
+echo "I'm executing $command"
+command=$(echo "$command"| tr "[:upper:]" "[:lower:]" 2>&1)
+echo "New transformed command is $command"
 #execute said command
+for item in ${command[@]}; do
+	echo "The command in array which the user input is being compared to is ${item}" 
+	if [ "${item}" = "${command}" ]; then
+		check_${command}
+		echo "Found a match for the command"		
+	fi
+done
 
 #file type count
 #go through all the files in repo and count how many they are
